0b2838fe6cf8b1877896b37a4aa0444fe9857ece
Shubham Aher <skaedu6502@gmail.com>
2025-10-22 11:39:20 +0530

    2nd change

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import otpRoutes from "./routes/otpRoute.js";
-import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error", err));
-app.use("/api/auth", authRoutes);
 
 app.use("/api", otpRoutes);
 
