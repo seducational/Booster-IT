2c14f7dcf52141574fed7eb3f8c6d27b78bbf025
Shubham Aher <skaedu6502@gmail.com>
2025-10-26 22:32:14 +0530

    two

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "https://boosterera-frontend.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],//good to GO
+  origin: "https://boosterera-frontend.vercel.app" ,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 }));
 app.use(express.json());
 
